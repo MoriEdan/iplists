442fdb47b7e6bc8d820b29d8243dfa7423e836cb
slick2 <carey.dayrit@gmail.com>
2018-12-23 23:58:19 +0800

    API key management

--- application/controllers/Api_man.php
+++ application/controllers/Api_man.php
@@ -7,13 +7,14 @@ class Api_man extends MY_Controller {
     public function __construct() {
         parent::__construct();
         if (!$this->ion_auth->logged_in()) {
-            
+            redirect();
         }
+        $this->load->model('keys_model');
     }
 
     public function index() {
         $data = array();
-
+        $data['keys'] = $this->keys_model->findAll();
         $data['content'] = $this->load->view('api_man/index', $data, TRUE);
         $this->render($data);
     }
--- application/views/api_man/index.php
+++ application/views/api_man/index.php
@@ -3,11 +3,13 @@
         $('#create-api').click(function () {
             $.ajax({
                 url: "/api/key/",
-                type: "PUT",               
-                headers: {"X-API-KEY":"admin"},
+                data:
+                type: "PUT",
+                headers: {"X-API-KEY": "4cccscwckgs00008s8ssoo04wc88k8k0sggk0k0k"},
                 dataType: "json",
                 success: function () {
                     //update the result set's
+                    location.reload();
                 }
 
             });
@@ -18,7 +20,15 @@
 
 <div class="row">
     <div class="col-md-12">
+        <h1>API keys</h1>
         <button class="btn btn-warning" id="create-api">Create API Key</button>
+
+        <table class="table table-condensed table-striped">
+            <?php foreach($keys as $key):?>
+            <tr><td><?php echo $key['key']?></td></tr>
+            <?php endforeach; ?>
+
+        </table>
 
     </div>
 </div>
