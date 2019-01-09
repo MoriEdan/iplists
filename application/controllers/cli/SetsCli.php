98cddb18678fb07bc0a7a40c7f14e19b54675f5f
slick2 <carey.dayrit@gmail.com>
2019-01-09 20:40:49 +0800

    modification on import status

--- application/controllers/cli/SetsCli.php
+++ application/controllers/cli/SetsCli.php
@@ -23,51 +23,52 @@ class SetsCli extends CI_Controller {
         $files = $this->import_lists_model->findAll();
 
         foreach ($files as $file) {
+            echo 'Processing - ' . $file['link'] . "\n";
+            if ($file['status'] == 'waiting') {
+                $link = file_get_contents($file['link']);
+                $ipsets = explode("\n", $link);
+                //$ipsets = read_ipset($file['filename']);
+                $this->import_lists_model->save(array('status' => 'importing'), $file['id']);
+                $isDatacenter = $file['isDatacenter'] == 1 ? TRUE : FALSE;
+                $isProxy = $file['isProxy'] == 1 ? TRUE : FALSE;
+                $counter = 1;
+                foreach ($ipsets as $ip) {
+                    $ip_data = array();
+                    $ip = clean_ip($ip);
 
-            $ipsets = read_ipset($file['filename']);
-            echo 'Processing - ' . $file['filename'] . "\n";
-            $this->import_lists_model->save(array('status'=> 'importing'), $file['id']);
-            $isDatacenter = $file['isDatacenter'] == 1 ? TRUE : FALSE;
-            $isProxy = $file['isProxy'] == 1 ? TRUE : FALSE;
-            $counter = 1;
-            foreach ($ipsets as $ip) {
-                $ip_data = array();
-                $ip = clean_ip($ip);
-                
-                if (check_ip($ip)) {                    
-                    if (!$this->ip_lists_model->is_unique($ip)) {
-                        echo 'line ' . $counter . " {$ip} - already exists.\n";
-                        $counter++;
-                        continue;
+                    if (check_ip($ip)) {
+                        if (!$this->ip_lists_model->is_unique($ip)) {
+                            echo 'line ' . $counter . " {$ip} - already exists.\n";
+                            $counter++;
+                            continue;
+                        }
+                        //insert the IP                    
+                        $range = Range::parse($ip);
+                        $first_ip = $range->getFirstIP();
+                        $last_ip = $range->getLastIP();
+                        $ip_data = array(
+                            'ip' => $ip,
+                            'first_ip' => (string) $first_ip,
+                            'last_ip' => (string) $last_ip,
+                            'isDatacenter' => $isDatacenter,
+                            'isProxy' => $isProxy
+                        );
+
+                        $this->ip_lists_model->insert($ip_data);
+                        echo 'line ' . $counter . " {$ip} - added.\n";
                     }
-                    //insert the IP                    
-                    $range = Range::parse($ip);
-                    $first_ip = $range->getFirstIP();
-                    $last_ip = $range->getLastIP();
-                    $ip_data = array(
-                        'ip' => $ip,
-                        'first_ip' => (string) $first_ip,
-                        'last_ip' => (string) $last_ip,
-                        'isDatacenter' => $isDatacenter,
-                        'isProxy' => $isProxy
-                    );
-                   
-                    $this->ip_lists_model->insert($ip_data);
-                    echo 'line ' . $counter . " {$ip} - added.\n";
-                   
+                    $counter++;
                 }
-                $counter++;
             }
-            echo $file['filename'] . ' - done'."\n";
-            $this->import_lists_model->save(array('status'=> 'done'), $file['id']);
+            echo $file['link'] . ' - done' . "\n";
+            $this->import_lists_model->save(array('status' => 'done'), $file['id']);
             //$this->import_lists_model->remove($file['id']);
-            unlink("{$file['filename']}");
+            //unlink("{$file['filename']}");
         }
-        echo 'Import Done'."\n";
-    }
-    
-    public function remove(){
         
+    }
+
+    public function remove() {
         
     }
 
